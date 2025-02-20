d8741be9ce700e25efba51fc0b02a0de836de2b4
Prasenjit <prasenjit.aluni@gmail.com>
2025-02-20 10:07:12 +0530

    NestJS & Mysql : Setup-01

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]), 
+    UsersModule
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {Repository} from "typeorm";
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+    async findOne(condition: any): Promise<User> {
+        //return this.userRepository.findOneBy(condition)
+        //return this.userRepository.findOneByOrFail(condition)
+        return this.userRepository.findOneByOrFail({"id":20})
+    }
+}
